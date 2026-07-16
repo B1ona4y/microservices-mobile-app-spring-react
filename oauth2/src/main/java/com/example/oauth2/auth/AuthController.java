55d35bdda2ce7213324c85bfab77dcc40cb35a12
B1ona4y <eu.oleynich@gmail.com>
2026-07-16 17:45:47 +0200

    fix github oauth flow, remove unused import

--- oauth2/src/main/java/com/example/oauth2/auth/AuthController.java
+++ oauth2/src/main/java/com/example/oauth2/auth/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.oauth2.auth.AuthController.AuthResponse;
 import com.example.oauth2.user.User;
 import com.example.oauth2.user.UserService;
 
