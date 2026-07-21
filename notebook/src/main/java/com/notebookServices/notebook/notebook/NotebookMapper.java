ae1beb8553a41975eac3f5c94b64dc874fd99f9e
B1ona4y <eu.oleynich@gmail.com>
2026-07-21 17:09:08 +0200

    feat: notebook mapper repo and service

--- notebook/src/main/java/com/notebookServices/notebook/notebook/NotebookMapper.java
+++ notebook/src/main/java/com/notebookServices/notebook/notebook/NotebookMapper.java
@@ -1,5 +1,21 @@
 package com.notebookServices.notebook.notebook;
 
-public class NotebookMapper {
+import java.util.List;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import com.notebookServices.notebook.notebook.dto.NotebookRequest;
+import com.notebookServices.notebook.notebook.dto.NotebookResponse;
+
+@Mapper(componentModel = "spring")
+public interface NotebookMapper {
+    NotebookResponse toResponse(Notebook notebook);
+
+    List<NotebookResponse> tResponseList(List<Notebook> notebooks);
+
+    @Mapping(target = "owner", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Notebook toEntity(NotebookRequest request);
 }
--- notebook/src/main/java/com/notebookServices/notebook/notebook/NotebookService.java
+++ notebook/src/main/java/com/notebookServices/notebook/notebook/NotebookService.java
@@ -1,5 +1,25 @@
 package com.notebookServices.notebook.notebook;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotebookService {
 
+    private final NotebookRepository notebookRepository;
+
+    public NotebookService(NotebookRepository notebookRepository) {
+        this.notebookRepository = notebookRepository;
+    }
+
+    public Optional<Notebook> findByIdAndOwner(UUID id, String owner) {
+        return notebookRepository.findByIdAndOwner(id, owner);
+    }
+
+    public List<Notebook> findByOwnerAndDeletedFalse(String owner) {
+        return notebookRepository.findByOwnerAndDeletedFalse(owner);
+    }
 }
