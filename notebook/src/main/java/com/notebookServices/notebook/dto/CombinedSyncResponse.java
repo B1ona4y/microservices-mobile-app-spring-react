ad73493ae8fc4a7e1f6379df68e1cf55aaa777ed
B1ona4y <eu.oleynich@gmail.com>
2026-07-22 10:53:28 +0200

    feat: dtos for sync module

--- notebook/src/main/java/com/notebookServices/notebook/dto/CombinedSyncResponse.java
+++ notebook/src/main/java/com/notebookServices/notebook/dto/CombinedSyncResponse.java
@@ -1,5 +1,11 @@
 package com.notebookServices.notebook.dto;
 
-public record CombinedSyncResponse() {
+import com.notebookServices.notebook.notebook.Notebook;
+import com.notebookServices.notebook.notebook.pages.Page;
+import com.synccore.synccore.dto.SyncResponse;
 
-}
+
+public record CombinedSyncResponse(
+    SyncResponse<Notebook> notebooks,
+    SyncResponse<Page> pages
+) {}
