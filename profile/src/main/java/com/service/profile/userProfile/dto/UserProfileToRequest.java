9526e554978e5558fb21d284716caa5d8c15ccdb
Nurgl1ch <eu.oleynich@gmail.com>
2026-08-04 17:12:49 +0200

    fix: removed id and version from request dto

--- profile/src/main/java/com/service/profile/userProfile/dto/UserProfileToRequest.java
+++ profile/src/main/java/com/service/profile/userProfile/dto/UserProfileToRequest.java
@@ -1,7 +1,5 @@
 package com.service.profile.userProfile.dto;
 
-import java.util.UUID;
-
-public record UserProfileToRequest(UUID id, String displayName, String avatarUrl, String bio, long version) {
+public record UserProfileToRequest(String displayName, String avatarUrl, String bio) {
 
 }
