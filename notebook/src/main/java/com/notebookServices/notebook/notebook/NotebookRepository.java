04886ff30009cb07589669609dbdc2fdd1f8767b
Nurgl1ch <eu.oleynich@gmail.com>
2026-08-17 14:26:41 +0200

    feat: cascade deletion of pages

--- notebook/src/main/java/com/notebookServices/notebook/notebook/NotebookRepository.java
+++ notebook/src/main/java/com/notebookServices/notebook/notebook/NotebookRepository.java
@@ -1,5 +1,6 @@
 package com.notebookServices.notebook.notebook;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,4 +11,6 @@ public interface NotebookRepository extends SyncRepository<Notebook> {
     Optional<Notebook> findByIdAndOwner(UUID id, String owner);
 
     List<Notebook> findByOwnerAndDeletedFalse(String owner);
+
+    List<Notebook> findByIdInAndOwnerAndDeletedTrue(Collection<UUID> ids, String owner);
 }
